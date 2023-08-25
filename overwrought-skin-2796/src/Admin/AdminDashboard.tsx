2ad6fcd81d877c92b5732766ade259bb1a89de4c
Nagaraju0710 <nagarajulonka1999@gmail.com>
2023-08-25 22:26:16 +0530

    admin Complete

--- overwrought-skin-2796/src/Admin/AdminDashboard.tsx
+++ overwrought-skin-2796/src/Admin/AdminDashboard.tsx
@@ -2,12 +2,14 @@ import Chart from "../Logingif/Chart.gif"
 import img2 from "../Logingif/img2.png"
 import img3 from "../Logingif/img3.png"
 import giphy from "../Logingif/giphy.webp"
-import Navbar from "../Components/Navbar"
+import AdminNavbar from "./AdminNavbar"
+
 
 export const AdminDashboard = () => {
     return (
         <div>
-            <Navbar/>
+         <AdminNavbar/>
+
         <div style={{backgroundColor:"lightblue",height:"1200px",paddingTop:"50px"}}>
             <div style={{ display: "flex", gap: "50px", margin: "70px" }}>
                 <div style={{ width: "600px", height: "400px", boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px", borderRadius: '10px 10px' }}>
--- overwrought-skin-2796/src/Pages/AllRoutes.jsx
+++ overwrought-skin-2796/src/Pages/AllRoutes.jsx
@@ -4,6 +4,10 @@ import HomePage from './Homepage';
 import { Login } from './LoginPage';
 import { SignUpPage } from './SignUpPage';
 import { Dashboard } from './Dasboard';
+import { AdminDashboard } from '../Admin/AdminDashboard';
+import PrivateRoutes from '../Components/PrivateRoute';
+
+import { UserDetails } from '../Admin/UserDetails';
 
 const AllRoutes = () => {
   return (
@@ -12,6 +16,8 @@ const AllRoutes = () => {
       <Route path='/login' element={<Login/>}/>
       <Route path='/signup' element={<SignUpPage/>}/>
       <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/a/users' element={<UserDetails/>}/>
+      <Route path="/a/dashboard" element={<PrivateRoutes><AdminDashboard/></PrivateRoutes>}/>
       <Route path='*' element={<h3>Page Not Found 404</h3>}/>
     </Routes>
   )
