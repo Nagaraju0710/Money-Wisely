5e0a33b5f97dbd41e13ad0630cb7ec20cfda7fc2
verma076 <nvmasai0706@gmail.com>
2023-08-25 22:59:35 +0530

    conflict

--- overwrought-skin-2796/src/Pages/AllRoutes.jsx
+++ overwrought-skin-2796/src/Pages/AllRoutes.jsx
@@ -3,9 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from './Homepage';
 import { Login } from './LoginPage';
 import { SignUpPage } from './SignUpPage';
-import { Dashboard } from './Dasboard';
+// import { Dashboard } from './Dasboard';
 import {ContactUs} from "./ContactUs"
-import { Dashboard } from './Dashboard';
+// import { Dashboard } from './Dashboard';
 import { Profile } from '../DashBoardComponents/Profile';
 import { Analysis } from '../DashBoardComponents/Analysis';
 import { Income } from '../DashBoardComponents/Income';
@@ -17,7 +17,7 @@ const AllRoutes = () => {
       <Route path='/' element={<HomePage/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/signup' element={<SignUpPage/>}/>
-      <Route path='/dashboard' element={<Dashboard/>}/>
+      {/* <Route path='/dashboard' element={<Dashboard/>}/> */}
       {/* <Route path="/contact">{<ContactUs/>}</Route> */}
       <Route path='*' element={<h3>Page Not Found 404</h3>}/>
 
